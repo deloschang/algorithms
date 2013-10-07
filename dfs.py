ab67422c361c7a76fb3f28f9709ea30ca2d408b9
Delos Chang <delosfchang@gmail.com>
2013-10-07 16:02:38 -0400

    adding dfs algorithm for edge classification

---
+++ dfs.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python
+
+graph = {'A': ['G', 'E', 'B'],
+             'B': ['E', 'H'],
+             'G': ['F', 'E'],
+             'E': [],
+             'F': ['A'],
+             'C': ['B', 'H', 'D'],
+             'H': ['E'],
+             'D': ['E']}
+
+color = {}
+pi = {}
+
+d = {}
+f = {}
+glob_time = 0 
+
+
+edges = []
+
+def DFSVisit(u):
+    global glob_time
+    color[u] = "gray"
+
+    glob_time += 1
+    d[u] = glob_time
+
+    for v in graph[u]:
+        print "Exploring vertices adjacent to " + str(v)
+
+        if color[v] == "white":
+            pi[v] = u
+
+            DFSVisit(v)
+
+    color[u] = "black"
+
+    glob_time += 1
+    f[u] = glob_time
+
+
+def DFS(graph):
+    global glob_time
+    for u in graph:
+        color[u] = "white"
+        pi[u] = "nil"
+    
+    for u in graph:
+        print "Starting DFS from " + str(u)
+        if color[u] == "white":
+            DFSVisit(u)
+
+def create_edges(graph):
+    for u in graph:
+        for v in graph[u]:
+            dict_m = {u:v}
+            edges.append(dict_m)
+    
+    print edges
+
+def print_edges(graph):
+    for vert in edges:
+        u = vert.keys()[0]
+        v = vert.values()[0]
+
+        if pi[v] == u:
+            print "Tree Edge: (" + pi[v] + ", " + v + ")"
+        else:
+            # v is adj to u 
+            # (u, v)
+            if d[u] < d[v] and d[v] < f[v] and f[v] < f[u]:
+                # v is a desc of u 
+                print "Forward Edge: (" + u + ", " + v + ")"
+            elif d[v] < d[u] and d[u] < f[u] and f[u] < f[v]:
+                # u is a desc of v 
+                print "Back Edge: (" + u + ", " + v + ")"
+            else:
+                print "Cross Edge: (" + u + ", " + v + ")"
+
+DFS(graph)
+print "=========== Done ============"
+create_edges(graph)
+print_edges(graph)
+
+print pi
+print d
+
+
