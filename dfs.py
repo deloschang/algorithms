5f09fb76653782e1ab43a182f64063ade9f137c0
Delos Chang <delosfchang@gmail.com>
2013-10-07 16:21:44 -0400

    adding another graph representing figure 22.8

--- dfs.py
+++ dfs.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 
-# This python program solves the following problems:
+# Author: Delos Chang
+
+# This python program solves the following CLRS algorithm problems:
+
 # CLRS 22.3-10:
     # Modify the pseudocode for depth-first search so that it prints out every edge in the directed graph 
     # G, together with its type. Show what modifications, if any, you need to make if G is undirected.
@@ -10,17 +13,35 @@
     # and two vertices s and t, and returns the number of simple paths from s to t in G.
 
 
-graph = {'A': ['G', 'E', 'B'],
-             'B': ['E', 'H'],
-             'G': ['F', 'E'],
-             'E': [],
-             'F': ['A'],
+graph = {'S': ['G', 'T', 'B'],
+             'B': ['T', 'H'],
+             'G': ['F', 'T'],
+             'T': [],
+             'F': ['S'],
              'C': ['B', 'H', 'D'],
-             'H': ['E'],
-             'D': ['E']}
+             'H': ['T'],
+             'D': ['T']}
+
+graph = {
+
+             'P': ['O','S','Z'],
+            'M': ['R', 'Q', 'T'],
+             'N': ['Q', 'U', 'O'],
+             'O': ['R', 'V', 'S'],
+             'Q': ['T'],
+             'R': ['U', 'Y'],
+             'S': ['R'],
+             'T': [],
+             'U': ['T'],
+             'V': ['X', 'W'],
+             'X': [],
+             'Z': [],
+             'Y': ['V']}
 
 color = {}
 pi = {}
+
+count = {} # container for CLRS 22.4-2
 
 d = {}
 f = {}
@@ -39,12 +60,23 @@ def DFSVisit(u):
     for v in graph[u]:
         print "Exploring vertices adjacent to " + str(v)
 
+        # Discovery of vertex
         if color[v] == "white":
             pi[v] = u
 
-            DFSVisit(v)
+            # Each time DFS discovers a new vertex, if it is T, set its count to 1. Otherwise, set it to 0.
+            # If the vertex discovered is T, set it to "finished" and do not continue DFS from t
+            if v == "V":
+                count[v] = 1
+                color[v] = "black"
+            else:
+                DFSVisit(v)
+
+        # Each time u finishes with a vertex v, add the count to vertex u
+        count[u] += count[v]
 
     color[u] = "black"
+
 
     glob_time += 1
     f[u] = glob_time
@@ -55,11 +87,13 @@ def DFS(graph):
     for u in graph:
         color[u] = "white"
         pi[u] = "nil"
+        count[u] = 0 ## 22.4-2 solution
     
     for u in graph:
         print "Starting DFS from " + str(u)
         if color[u] == "white":
             DFSVisit(u)
+
 
 def create_edges(graph):
     for u in graph:
@@ -69,6 +103,7 @@ def create_edges(graph):
     
     print edges
 
+# Solution for 22.3-10
 def print_edges(graph):
     for vert in edges:
         u = vert.keys()[0]
@@ -88,12 +123,20 @@ def print_edges(graph):
             else:
                 print "Cross Edge: (" + u + ", " + v + ")"
 
-DFS(graph)
-print "=========== Done ============"
-create_edges(graph)
-print_edges(graph)
+# Solution for 22.4-2
+# returns the count from s to t
+def calc_count():
+    print count['P']
 
-print pi
-print d
+
+DFS(graph2)
+create_edges(graph2)
+print "=========== Done ============"
+#print_edges(graph)
+
+print "=========== 22.4-2 ============"
+calc_count()
+
+
 
 
